515620bec4495b932776bb5ee86d3902f3278cad
sarnali15 <khatuna15-3515@diu.edu.bd>
2024-05-11 03:58:02 +0600

    add queries form updated

--- src/Pages/AddQueries/AddQueries.jsx
+++ src/Pages/AddQueries/AddQueries.jsx
@@ -1,9 +1,49 @@
+import { useContext } from "react";
+import { AuthContext } from "../../Provider/AuthProvider";
+import axios from "axios";
 
 
 const AddQueries = () => {
+    const { user } = useContext(AuthContext);
+
+    const handleAddQueries = async e => {
+        e.preventDefault();
+        const form = e.target;
+        const queryTitle = form.title.value;
+        const queryBrand = form.brand.value;
+        const productName = form.pname.value;
+        const queryPhoto = form.photo.value;
+        const queryReason = form.boycottReason.value;
+        const userName = user?.displayName;
+        const userEmail = user?.email;
+        const userImage = user?.photoURL;
+        // const currentTime = time;
+        const currentTime = '2:14 AM Sunday, 11 May 2024';
+        const recommendationCount = 0;
+
+        const queryData = {
+            queryTitle,
+            queryBrand,
+            productName,
+            queryPhoto,
+            queryReason,
+            userName,
+            userEmail,
+            userImage,
+            currentTime,
+            recommendationCount
+        }
+        console.table(queryData);
+        try {
+            const { data } = await axios.post(`${import.meta.env.VITE_API_URL}/queries`, queryData)
+            console.log(data)
+        } catch (err) {
+            console.log(err)
+        }
+    }
     return (
         <div className="max-w-4xl mx-auto">
-            <form className="card-body bg-emerald-100 my-5 rounded">
+            <form onSubmit={handleAddQueries} className="card-body bg-emerald-100 my-5 rounded">
                 <h1 className="text-2xl font-bold text-center text-emerald-800 py-5">Add Your Queries</h1>
                 <div className="md:flex md:gap-3">
                     <div className="form-control w-full">
